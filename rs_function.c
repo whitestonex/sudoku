56a914398dfca696de20f9be1e006965792aaa6a
whitestonex <cherubminney@gmail.com>
2016-09-18 21:32:50 +0800

    add load data and init data in memory

--- rs_function.c
+++ rs_function.c
@@ -18,11 +18,77 @@
 
 
 
+static void initData(void)
+{
+    int i,j,k,l,x,y;
+
+    for (i = 0; i < 9; i++)
+    {
+        for (j = 0; j < 9; j++)
+        {
+            if (sudokuBt[i][j].iValue != 0)
+            {
+                sudokuBt[i][j].iPossibleCount = 0;
+                sudokuBt[i][j].iPossibleType = 0;
+            }
+            else
+            {
+                sudokuBt[i][j].iPossibleCount = 9;
+                sudokuBt[i][j].iPossibleType = 0x1FF;
+                for (k = 0; k < 9; k++)
+                {
+                    sudokuBt[i][j].arrayPossibleFlag[k] = 1;
+                    sudokuBt[i][j].arrayPossibleValue[k] = k+1;
+                }
+            }
+        }
+    }
+
+    memset(&pCheckList,0x0,sizeof(pCheckList));
+
+    k = 0;
+    for (i = 0; i < 9; i++)
+    {
+        for (j = 0; j < 9; j++)
+        {
+            pCheckList[k][j] = &sudokuBt[i][j];
+        }
+        k++;
+    }
+
+    for (j = 0; j < 9; j++)
+    {
+        for (i = 0; i < 9; i++)
+        {
+            pCheckList[k][i] = &sudokuBt[i][j];
+        }
+        k++;
+    }
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            l = 0;
+            for (x = 0; x < 3; x++)
+            {
+                for (y = 0; y < 3; y++)
+                {
+                    pCheckList[k][l++] = &sudokuBt[3*i+x][3*j+y];
+                }
+            }
+            k++;
+        }
+    }
+}
+
+
 
 int rsLoadData(char *pcDataFile)
 {
     FILE *pF = NULL;
     char cLine[32];
+    int i, j;
 
     pF = fopen(pcDataFile, "r");
     if (pF == NULL)
@@ -31,23 +97,31 @@ int rsLoadData(char *pcDataFile)
         return -1;
     }
 
+    memset(&sudokuBt[0][0], 0x0, sizeof(sudokuBt));
+
+    i = 0;
     while(!feof(pF))
     {
         memset(cLine, 0x0, sizeof(cLine));
         fgets(cLine, sizeof(cLine), pF);
 
+        for (j = 0; j < 9; j++)
+        {
+            sudokuBt[i][j].iValue = cLine[2*j]-'0';
+        }
+        i++;
     }
 
-
-
     fclose(pF);
+
+    initData();
 
     return 0;
 }
 
 void rsPrintSudoku(void)
 {
-    int i, j;
+    int i, j, k;
 
     for (i = 0; i < 9; i++)
     {
@@ -57,4 +131,22 @@ void rsPrintSudoku(void)
         }
         printf("\n");
     }
+
+#if 0
+    printf("========\n");
+
+    for (k = 0; k < 3 ; k++)
+    {
+        for (i = 0; i < 9; i++)
+        {
+            for (j = 0; j < 9; j++)
+            {
+                printf("%d  ", pCheckList[9*k+i][j]->iValue);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
+#endif
+
 }
